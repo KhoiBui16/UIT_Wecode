4841acc6f6a4d53abb6aa6fe19f2439e1542a404
KhoiBui <156224787+KhoiBui16@users.noreply.github.com>
2024-05-03 10:10:05 +0700

    DSA exercises

---
+++ File_wecode/Data Structure Algoritm/Week5/Template/Bai_01/template_01.cpp
@@ -0,0 +1,84 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+define
+include
+using
+std
+###End banned keyword*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+	
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+                }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void preOrder(Node *root) {
+//###INSERT CODE HERE -
+    }
+
+}; //End of Solution
+
+int main() {
+    
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    myTree.preOrder(root);
+
+    return 0;
+}
