228aa8c1956d3d2a184cc70d3121bb8f8e50494f
KhoiBui <156224787+KhoiBui16@users.noreply.github.com>
2024-07-01 21:44:55 +0700

    mock test files

---
+++ File_wecode/Data Structure Algoritm/Mock Test/Cây nhị phân - Kiểm tra tính cân bằng tại nút gốc/template.cpp
@@ -0,0 +1,52 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+include
+###End banned keyword*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct TNode {
+    int key;
+    TNode *left, *right;
+};
+
+typedef TNode* TREE;
+
+TREE CreateTree(vector<int> pre, vector<int> in, int preB, int preE, int inB, int inE) {
+	int i;
+	TREE root;
+	if (inE < inB) return NULL;
+	root = new TNode;
+	if (root != NULL) {
+		root->key = pre[preB];
+		for (i = inB; i <= inE; i++)
+			if (in[i] == pre[preB]) break;
+		root->left = CreateTree(pre, in, preB+1, preE, inB, i - 1);
+		root->right = CreateTree(pre, in, preB+i-inB+1, preE, i+1,inE);
+	} return root;
+}
+
+void Input(vector<int> &v)
+{
+	int tmp;
+	cin >> tmp;
+	while (tmp > 0) {
+		v.push_back(tmp);
+		cin >> tmp;
+	}
+}
+
+//###INSERT CODE HERE -
+
+int main() {
+    vector<int> duyetNLR;
+    vector<int> duyetLNR;
+    Input(duyetNLR);
+    Input(duyetLNR);
+    int Num=duyetNLR.size()-1;
+    TREE root = CreateTree(duyetNLR, duyetLNR, 0, Num, 0, Num);
+    Fun(root);
+    return 0;
+}
