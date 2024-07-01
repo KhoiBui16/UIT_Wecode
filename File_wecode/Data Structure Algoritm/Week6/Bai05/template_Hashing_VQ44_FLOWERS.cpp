127fadfffb20fa0688c04bc0e70bd8bab2f818db
KhoiBui <156224787+KhoiBui16@users.noreply.github.com>
2024-07-01 21:40:40 +0700

    source files

---
+++ File_wecode/Data Structure Algoritm/Week6/Bai05/template_Hashing_VQ44_FLOWERS.cpp
@@ -0,0 +1,37 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+sort
+stable_sort
+partial_sort
+partial_sort_copy
+sort_heap
+set
+multi_set
+merge
+###End banned keyword*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+vector<int> a;
+
+
+vector<int> get_ans(const vector<int>& A,int K){
+	map<int,int> Hash;
+//###INSERT CODE HERE -
+	
+}
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+	
+	cin >> n >> k;
+	
+	a.resize(n);
+	for (int i = 0; i < n; i++) cin >> a[i];
+	
+	vector<int> ans = get_ans(a,k);
+	
+	for (const int& x: ans) cout << x << ' ';
+	return 0;
+}
---
+++ File_wecode/Data Structure Algoritm/Week6/Bai06/template_Hasing_KiemKe.cpp
@@ -0,0 +1,36 @@
+/*###Begin banned keyword - each of the following line if appear in code will raise error. regex supported
+sort
+stable_sort
+partial_sort
+partial_sort_copy
+sort_heap
+set
+multi_set
+merge
+###End banned keyword*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int N;
+int count_distinct(const vector<string>& ids){
+
+	map<string,int> Hash;
+
+//###INSERT CODE HERE -
+
+}
+
+int main(){
+	ios::sync_with_stdio(false); cin.tie(nullptr);
+
+	cin >> N;
+	vector<string> ids(N);
+
+	for (int i = 0; i < N; i++) cin >> ids[i];
+
+	cout << count_distinct(ids);
+
+	return 0;
+}
